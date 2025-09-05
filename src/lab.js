0f4d5e05c7e154c1d4f4c45cf2b19520f2f1e469
Grace <gracelee.dev@gmail.com>
2025-09-04 21:19:22 -0400

    Completed lab demo tasks

--- src/lab.js
+++ src/lab.js
@@ -3,15 +3,23 @@
 // Input: [1, 2, 3, 4]
 // Output: [4, 3, 2, 1]
 function reverseArray(arr) {
-  // Write you solution here
+  const reverseArray = [];
+
+  for (let i = arr.length; i > 0; i--) {
+    reverseArray.push(arr[i - 1]);
+  }
+  // console.log(reverseArray);
+  // //output: [ 4, 3, 2, 1 ]
+
+  return reverseArray;
 }
 
 // Write a function that removes duplicate numbers from an array and returns a new array with only unique values.
 // Example:
 // Input: [1, 2, 2, 3, 4, 4, 5]
 // Output: [1, 2, 3, 4, 5]
-function removeDuplicates(arr) {
-  // Write you solution here
+function removeDuplicates(array) {
+  return Array.from(new Set([...array]));
 }
 
 // Write a function that sorts an array of numbers in ascending order (smallest to largest).
@@ -19,17 +27,30 @@ function removeDuplicates(arr) {
 // Input: [5, 2, 9, 1, 5, 6]
 // Output: [1, 2, 5, 5, 6, 9]
 function sortArray(arr) {
-  // Write you solution here
+  const sortedArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+  const tempSortedArray =  arr.sort()
+  sortedArray.push(tempSortedArray[i]);    
+  }
+
+  return sortedArray;
 }
 
 // You have a synchronous function that returns a greeting message with your name.
 // Your task is to convert it into an asynchronous function that returns a Promise,
+
+
+
 function syncGreet(name) {
   return `Hello, ${name}!`;
 }
 
 function asyncGreet(name) {
-  // Write you solution here
+  const promise1 = new Promise((resolve, reject) => {
+    resolve(`Hello, ${name}!`);
+  });
+  return promise1;
 }
 
 module.exports = {
